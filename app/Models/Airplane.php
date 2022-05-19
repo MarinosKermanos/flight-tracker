96be4f65fa105c4118d5f9e65362daf53286b9c7
Marinos Kermanos <marinos@circlelinkhealth.com>
2022-05-19 16:50:48 +0300

    Adds many to many migration

--- app/Models/Airplane.php
+++ app/Models/Airplane.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Airplane extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function flight()
+    {
+        return $this->hasMany(Flight::class);
+    }
+
+    public function airports()
+    {
+        return $this->belongsToMany(Airport::class,'airplane_airport','airplane_id','airport_id');
+    }
+
 }
