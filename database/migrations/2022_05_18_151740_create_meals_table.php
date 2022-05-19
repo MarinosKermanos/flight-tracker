dcda3ca1b2453bb5b0742cb8ebf17b101c2acada
Marinos Kermanos <marinos@circlelinkhealth.com>
2022-05-19 12:45:17 +0300

    Adds Meal model controller migration

---
+++ database/migrations/2022_05_18_151740_create_meals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_vegetarian');
+            $table->unsignedBigInteger('chef_user_id');
+            $table->timestamps();
+
+            $table->foreign('chef_user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+};
